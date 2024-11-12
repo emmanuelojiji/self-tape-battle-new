5348a54acb86a9f43fb99055e83e24b699ccdbf5
emmanuelojiji <emmanuelojiji@hotmail.com>
2024-11-12 22:46:49 +0000

    feat: confetti explosion added to reward model

--- src/components/RewardModal.js
+++ src/components/RewardModal.js
@@ -1,12 +1,17 @@
+import ConfettiExplosion from "react-confetti-explosion";
 import "./RewardModal.scss";
 
 const RewardModal = ({ coinsEarned }) => {
   return (
-    <div className="reward-modal-container">
-    <div className="RewardModal">
-      <h3>You've earned {coinsEarned} coin!</h3>
+    <>
+      <div className="reward-modal-container">
+        <div className="RewardModal">
+          <h3>You've earned {coinsEarned} coin!</h3>
+          <ConfettiExplosion zIndex={2} />
+          <button>Close</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
