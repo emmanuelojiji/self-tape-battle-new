47001c44855544cd26efad2370bbb19068423b46
emmanuelojiji <emmanuelojiji@hotmail.com>
2024-11-26 08:02:58 +0000

    feat: amount of coins added to wallet page

--- src/pages/wallet/Wallet.js
+++ src/pages/wallet/Wallet.js
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import Header from "../../components/Header";
+import { UserContext } from "../../contexts/UserContext";
 
 const Wallet = () => {
+  const { coins } = useContext(UserContext);
   return (
     <div className="Wallet">
       <Header />
       <div className="page-container">
         <h1>Wallet</h1>
+        <h2>{coins} coins</h2>
       </div>
     </div>
   );
